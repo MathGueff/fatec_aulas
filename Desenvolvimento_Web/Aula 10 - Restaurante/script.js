9b9a611aa134d4c579d8bb30b58021a37d970f1c
Matheus <gueffmatheus@gmail.com>
2024-04-23 22:23:22 -0300

    Feito o código da lanchonete da aula 10 de DW

---
+++ Desenvolvimento_Web/Aula 10 - Restaurante/script.js
@@ -0,0 +1,39 @@
+let adicionais=0;
+let entrega = 0;
+
+function totalPagar(){
+    let total = 0
+   
+    calcularAdicionais();
+    total = adicionais + entrega
+    //alert("O total a pagar é "+ total)
+    resultado = document.getElementById("total").value
+    if(total != 0){
+        resultado = total
+    }else{
+        resultado = "Nenhum valor foi adicionado!"
+    }
+    document.getElementById("total").value = total
+}
+
+function calcularAdicionais(){
+    let selecionados = document.querySelectorAll("input[name='adicionais']");
+    adicionais = 0
+    let qtd = selecionados.length;
+
+    for(i=0;i<qtd;i++){
+        if(selecionados[i].checked){
+            console.log(selecionados[i].value)
+            adicionais += parseInt(selecionados[i].value)
+        }
+    }
+    if(adicionais == 0){
+        //alert("Nenhum adicional foi adicionado")
+    }else{
+         //alert(adicionais)
+    }
+}
+
+function calcularEntrega(){
+    
+}
