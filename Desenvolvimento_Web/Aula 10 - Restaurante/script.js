f9c74608cce99ba1636cfc08c928bd92a04e351b
Matheus <gueffmatheus@gmail.com>
2024-05-07 22:14:57 -0300

    Atualizado o código da lanchonetegit add .

--- Desenvolvimento_Web/Aula 10 - Restaurante/script.js
+++ Desenvolvimento_Web/Aula 10 - Restaurante/script.js
@@ -1,21 +1,41 @@
 let adicionais=0;
 let entrega = 0;
+let pedilanchedo = 0;
 
 function totalPagar(){
     let total = 0
-   
-    calcularAdicionais();
-    total = adicionais + entrega
-    //alert("O total a pagar é "+ total)
-    resultado = document.getElementById("total").value
-    if(total != 0){
-        resultado = total
-        document.getElementById("total").value = resultado+" reais"
-    }else{
-        resultado = "Nenhum valor foi adicionado!"
-        document.getElementById("total").value = resultado
+    verifCliente();
+    nome_cli = document.getElementById("nome").value
+    if(nome_cli == ""){
+        nome_cli = "Informe o seu nome"
     }
-    
+    telefone_cli = document.getElementById("telefone").value
+    if(telefone_cli == ""){
+        telefone_cli = "Nenhum telefone foi informado"
+    }
+    calcularPedido();
+    calcularAdicionais();
+    calcularEntrega();
+
+    mostra_total = document.getElementById("total");
+    descricao = document.getElementById("descricao");
+
+    total = adicionais + lanche + entrega;
+    //alert("O total a pagar é "+ total)
+    resultado = document.getElementById("total").value;
+    if(total != 0){
+        resultado = total;
+        mostra_total.value = `O total a pagar é ${total} reais`
+        descricao.innerHTML = `Nome do Cliente: ${nome_cli}\nTelefone de Contato: ${telefone_cli}\nLanche: ${lanche} reais \nAdicionais: ${adicionais} reais \nMétodo de Entrega ${entrega} reais`
+    }else{
+        resultado = "Nenhum valor foi adicionado!";
+        mostra_total.value = resultado;
+        descricao.innerHTML = ``
+    }
+}
+
+function verifCliente(){
+    nome_cli = document.getElementById("nome").value
 }
 
 function calcularAdicionais(){
@@ -29,13 +49,30 @@ function calcularAdicionais(){
             adicionais += parseInt(selecionados[i].value)
         }
     }
-    if(adicionais == 0){
-        //alert("Nenhum adicional foi adicionado")
-    }else{
-         //alert(adicionais)
-    }
 }
 
 function calcularEntrega(){
-    
+    let radioEntrega = document.querySelectorAll("input[name='escolha_entrega']");
+    let qtd = radioEntrega.length;
+
+    for(i=0;i<qtd;i++){
+        if(radioEntrega[i].checked){
+            entrega = Number(radioEntrega[i].value);
+            break;
+        }
+    }
 }
+
+function calcularPedido(){
+    lanche = Number(document.getElementById("combos").value);
+}
+
+function imprimir(){
+    window.print();
+}
+
+function clock(){
+    let Hora = new Date()
+    let mensagem = Hora.getHours() + ":" + Hora.getMinutes() +":" + Hora.getSeconds();
+    document.getElementById("relogio").innerHTML = mensagem;
+} setInterval(clock,1000)
