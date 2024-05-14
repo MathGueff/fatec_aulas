ce240b22e223f99aaf16f4586187e33ac100af98
Matheus <gueffmatheus@gmail.com>
2024-05-13 21:36:17 -0300

    Adicionado o arquivo do trabalho em grupo de ALP

---
+++ Algoritmo_Logica_de_Programacao/Aula13 - ExercicioFuncoes/menu.py
@@ -0,0 +1,16 @@
+from funcoes import *
+def main():
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+        if opcao == '1':
+            cadastrar()
+        elif opcao == '2':
+            exibir_mensagem()
+        elif opcao == '3':
+            print("Bye Bye!")
+            break
+        else:
+            print("Opção inválida!")
+
+main()
